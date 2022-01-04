1083e846b837acc30ecaa7935e6fc0770e7e7fc9
Mohit Kumar <mohitbarawal@gmail.com>
2022-01-04 08:35:56 +0530

    margin change

--- test.js
+++ test.js
@@ -119,34 +119,14 @@
     setInterval(update, 1000 / 60);
   }).call(this);
 
-  function makeTimer() {
+
+  //for coutdown timer
 
     //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-      var endTime = new Date("09 January 2022 11:00:00 GMT+01:00");			
-        endTime = (Date.parse(endTime) / 1000);
+      // var endTime = new Date("04 January 2022 08:29:00 GMT+01:00");			
+      //   endTime = (Date.parse(endTime) / 1000);
   
-        var now = new Date();
-        now = (Date.parse(now) / 1000);
-  
-        var timeLeft = endTime - now;
-  
-        var days = Math.floor(timeLeft / 86400); 
-        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-    
-        if (hours < "10") { hours = "0" + hours; }
-        if (minutes < "10") { minutes = "0" + minutes; }
-        if (seconds < "10") { seconds = "0" + seconds; }
-  
-        $("#days").html(days + "<span>Days</span>");
-        $("#hours").html(hours + "<span>Hours</span>");
-        $("#minutes").html(minutes + "<span>Minutes</span>");
-        $("#seconds").html(seconds + "<span>Seconds</span>");		
-  
-    }
-  
-    setInterval(function() { makeTimer(); }, 1000);
+
 
     (function () {
       const second = 1000,
@@ -161,7 +141,7 @@
           mm = String(today.getMonth() + 1).padStart(2, "0"),
           yyyy = today.getFullYear(),
           nextYear = yyyy ,
-          dayMonth = "01/11/",
+          dayMonth = "01/09/",
           birthday = dayMonth + yyyy;
       
       today = mm + "/" + dd + "/" + yyyy;
