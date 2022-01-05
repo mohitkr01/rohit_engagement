5d0f9e7dae5084b0b3c7b708257292597d156cf9
Mohit Kumar <mohitbarawal@gmail.com>
2022-01-05 11:34:20 +0530

    css changes

--- test.js
+++ test.js
@@ -136,23 +136,9 @@
     
       //I'm adding this section so I don't have to keep updating this pen every year :-)
       //remove this if you don't need it
-      let today = new Date(),
-          dd = String(today.getDate()).padStart(2, "0"),
-          mm = String(today.getMonth() + 1).padStart(2, "0"),
-          yyyy = today.getFullYear(),
-          nextYear = yyyy ,
-          dayMonth = "01/09/",
-          birthday = dayMonth + yyyy;
-      
-      today = mm + "/" + dd + "/" + yyyy;
-      if (today > birthday) {
-        birthday = dayMonth + nextYear;
-      }
       //end
-      
-      const countDown = new Date(birthday).getTime(),
+      const countDown = new Date("Jan 9, 2022 11:00:00").getTime(),
           x = setInterval(function() {    
-    
             const now = new Date().getTime(),
                   distance = countDown - now;
     
